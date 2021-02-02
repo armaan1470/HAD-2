6ebec8cccd75a11cb2e6a35a6a6514e2e2ca9cb7
armaan1470 <61717473+armaan1470@users.noreply.github.com>
2021-02-02 13:32:31 +0530

    Add files via upload

---
+++ src/Table.js
@@ -0,0 +1,23 @@
+const Table = () => {
+    return ( 
+        <table border="2">
+            <tr>
+                <td>Armaan</td>
+                <td>sycs</td>
+                <td>355</td>
+            </tr>
+            <tr>
+                <td>Armaan</td>
+                <td>sycs</td>
+                <td>355</td>
+            </tr>
+            <tr>
+                <td>Armaan</td>
+                <td>sycs</td>
+                <td>355</td>
+            </tr>
+        </table>
+     );
+}
+ 
+export default Table;
